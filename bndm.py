0b0e84ad29c46df0af1cfa1a3aacc83ed1769bae
Jake Kaplan <jakkap@stanford.edu>
2020-06-01 05:23:33 -0400

    Implemented visual simulation with demo

--- bndm.py
+++ bndm.py
@@ -1,37 +1,55 @@
 import numpy as np
+from time import sleep
 
 # def set_bit(mask, bitnum):
 #     mask[bitnum >> 5] |= 1 << (bitnum & 31)
 #     return mask
 
-# def get_mask(masks, c):
-#     if c in masks:
-#         return masks[c]
-#     return 0
+ULINE = '\033[4m'
+PLAIN = '\033[0m'
 
-def bndm(txt, pat):
+def bndm(txt, pat, verbose=False):
     n = len(txt)
     m = len(pat)
     assert m <= 64
     masks = np.zeros(256, dtype=np.int64)
     for i,c in enumerate(pat):
         masks[ord(c)] |= 1 << (m-i-1)
-        # print(bin(masks[ord(c)]))
     
     i = 0
     while i <= n-m:
         state = (1<<m)-1
-        # print(bin(state))
         shift_to = i+m
         j = i+m-1
+        if verbose:
+            matchstr = np.array([' ']*m)
+
         while state != 0:
             if j < i:
                 return txt[i:]
-            state &= masks[ord(txt[j])] # get_mask(masks, txt[j])
+            state &= masks[ord(txt[j])]
+            if verbose:
+                matchstr[np.array(list(format(state,'0'+str(m)+'b')))=='1'] = txt[j]
+                print(' '*i + pat)
+                print(' '*i + ''.join(matchstr))
+                print(txt[:j] + ULINE + txt[j:i+m] + PLAIN + txt[i+m:])
+                sleep(1)
             state <<= 1
-            # print(bin(state))            
             if state >> m:
                 shift_to = j
             j -= 1
+
+        if verbose:
+            print(' '*shift_to + ULINE + pat[:i+m-shift_to] + PLAIN + pat[i+m-shift_to:])
+            print()
+            print(txt[:shift_to] + ULINE + txt[shift_to:i+m] + PLAIN + txt[i+m:])
+            sleep(1)
         i = shift_to
     return ''
+
+def demo():
+    print('>>> ...' + bndm('acggtacga' \
+        + 'tcatgcgagtcgtagcaggttgatgt' \
+        + 'accggactgcacgctgatgctcgtag' \
+        + 'tgctcgatagtcgtagctgatcgatg' \
+        + 'ctcgata', 'tagct', True))
