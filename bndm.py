9ca612c9c1291050acb8b17822ee7dd9372629a7
Jake Kaplan <jakkap@stanford.edu>
2020-06-01 20:33:48 -0400

    First attempt k-approx matching - exact (k=0) still works

--- bndm.py
+++ bndm.py
@@ -11,34 +11,63 @@ PLAIN = '\033[0m'
 def uline(string):
     return ULINE + string + PLAIN
 
-def bndm(txt, pat, verbose=False):
+# Standard Approx Shift-AND Algorithm
+# def verify(txt, pat, k):
+#     states = np.array([(1<<b)-1 for b in range(k+1)], dtype=np.int64)
+
+
+def bndm(txt, pat, k=0, verbose=False):
     n = len(txt)
     m = len(pat)
-    assert m <= 64
+    assert m <= 63
     masks = np.zeros(256, dtype=np.int64)
     for i,c in enumerate(pat):
         masks[ord(c)] |= 1 << (m-i-1)
     
     i = 0
-    while i <= n-m:
-        state = (1<<m)-1
-        shift_to = i+m
-        j = i+m-1
+    while i <= n-m+k:
+        all_states = np.full(k+1, (1<<m)-1, dtype=np.int64)
+        states = np.array([(1<<b)-1 for b in range(1,k+2)], dtype=np.int64)
+        # print(all_states)
+        # print(states)
+        # state = (1<<m)-1
+        shift_to = i+m-k
+        j = shift_to-1
         if verbose:
             matchstr = np.array([' ']*m)
 
-        while state != 0:
+        while states[k] != 0 and j >= 0:
             if j < i:
-                return txt[i:]
-            state &= masks[ord(txt[j])]
-            if verbose:
-                matchstr[np.array(list(format(state,'0'+str(m)+'b')))=='1'] = txt[j]
-                print(' '*i + pat)
-                print(' '*i + ''.join(matchstr))
-                print(txt[:j] + uline(txt[j:i+m]) + txt[i+m:])
-                sleep(1)
-            state <<= 1
-            if state >> m:
+                if states[k] >> m:
+                    return txt[j+1:]
+            else:
+                prev = all_states[0]
+                all_states[0] &= masks[ord(txt[j])]
+                if verbose:
+                    matchstr[np.array(list(format(all_states[0],'0'+str(m)+'b')))=='1'] = txt[j]
+                    print(' '*i + pat)
+                    print(' '*i + ''.join(matchstr))
+                    print(txt[:j] + uline(txt[j:i+m]) + txt[i+m:])
+                    sleep(1)
+                all_states[0] <<= 1
+                for d in range(k):
+                    curr = all_states[d+1]
+                    all_states[d+1] &= masks[ord(txt[j])]
+                    all_states[d+1] |= (prev >> 1) | prev | all_states[d]
+                    all_states[d+1] <<= 1
+                    prev = curr
+            prev = states[0]
+            states[0] &= masks[ord(txt[j])]
+            states[0] <<= 1
+            for d in range(k):
+                curr = states[d+1]
+                states[d+1] &= masks[ord(txt[j])]
+                states[d+1] |= (prev >> 1) | prev | states[d]
+                states[d+1] <<= 1
+                prev = curr
+            # state &= masks[ord(txt[j])]
+            # state <<= 1
+            if all_states[k] >> m:
                 shift_to = j
             j -= 1
 
@@ -55,4 +84,4 @@ def demo():
         + 'tcatgcgagtcgtagcaggttgatgt' \
         + 'accggactgcacgctgatgctcgtag' \
         + 'tgctcgatagtcgtagctgatcgatg' \
-        + 'ctcgata', 'tagct', True))
+        + 'ctcgata', 'tagct', 0, True))
