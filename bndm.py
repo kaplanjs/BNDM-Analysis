6a4bc2a9656be8f4cb3fcdc56e496956e6310227
Jake Kaplan <jakkap@stanford.edu>
2020-06-02 13:42:11 -0400

    Implemented k-approximate bndm

--- bndm.py
+++ bndm.py
@@ -11,12 +11,12 @@ PLAIN = '\033[0m'
 def uline(string):
     return ULINE + string + PLAIN
 
-# Standard Approx Shift-AND Algorithm
-# def verify(txt, pat, k):
-#     states = np.array([(1<<b)-1 for b in range(k+1)], dtype=np.int64)
-
-
 def bndm(txt, pat, k=0, verbose=False):
+    if verbose and k:
+        print('Simulation currently not supported' \
+            + 'for k-approximate matching.')
+        verbose = False
+
     n = len(txt)
     m = len(pat)
     assert m <= 63
@@ -28,53 +28,59 @@ def bndm(txt, pat, k=0, verbose=False):
     while i <= n-m+k:
         all_states = np.full(k+1, (1<<m)-1, dtype=np.int64)
         states = np.array([(1<<b)-1 for b in range(1,k+2)], dtype=np.int64)
-        # print(all_states)
-        # print(states)
-        # state = (1<<m)-1
         shift_to = i+m-k
         j = shift_to-1
         if verbose:
             matchstr = np.array([' ']*m)
 
-        while states[k] != 0 and j >= 0:
-            if j < i:
-                if states[k] >> m:
-                    return txt[j+1:]
-            else:
-                prev = all_states[0]
-                all_states[0] &= masks[ord(txt[j])]
-                if verbose:
-                    matchstr[np.array(list(format(all_states[0],'0'+str(m)+'b')))=='1'] = txt[j]
-                    print(' '*i + pat)
-                    print(' '*i + ''.join(matchstr))
-                    print(txt[:j] + uline(txt[j:i+m]) + txt[i+m:])
-                    sleep(1)
-                all_states[0] <<= 1
-                for d in range(k):
-                    curr = all_states[d+1]
-                    all_states[d+1] &= masks[ord(txt[j])]
-                    all_states[d+1] |= (prev >> 1) | prev | all_states[d]
-                    all_states[d+1] <<= 1
-                    prev = curr
+        while all_states[k] and j >= max(0,i-2*k):
+            prev = all_states[0]
+            all_states[0] &= masks[ord(txt[j])]
+            if verbose:
+                matchstr[np.array(list(format(all_states[0],'0'+str(m)+'b')))=='1'] = txt[j]
+                print(' '*i + pat)
+                print(' '*i + ''.join(matchstr))
+                print(' '*k + txt[:j] + uline(txt[j:i+m-k]) + txt[i+m-k:])
+                sleep(1)
+            all_states[0] &= (1<<m)-1
+            all_states[0] <<= 1
+
+            for d in range(k):
+                curr = all_states[d+1]
+                all_states[d+1] &= masks[ord(txt[j])]
+                all_states[d+1] |= (prev >> 1) | prev | all_states[d]
+                all_states[d+1] &= (1<<m)-1
+                all_states[d+1] <<= 1
+                prev = curr
+
+            if all_states[k] >> m and j < shift_to:
+                    shift_to = max(i+1,j)
+
             prev = states[0]
             states[0] &= masks[ord(txt[j])]
+            # if verbose:
+            #     matchstr[np.array(list(format(states[0],'0'+str(m)+'b')))=='1'] = txt[j]
+            #     print(' '*i + pat)
+            #     print(' '*i + ''.join(matchstr))
+            #     print(' '*k + txt[:j] + uline(txt[j:i+m-k]) + txt[i+m-k:])
+            #     sleep(1)
             states[0] <<= 1
+
             for d in range(k):
                 curr = states[d+1]
                 states[d+1] &= masks[ord(txt[j])]
                 states[d+1] |= (prev >> 1) | prev | states[d]
                 states[d+1] <<= 1
                 prev = curr
-            # state &= masks[ord(txt[j])]
-            # state <<= 1
-            if all_states[k] >> m:
-                shift_to = j
+
+            if states[k] >> m:
+                return txt[j:]
             j -= 1
 
         if verbose:
-            print(' '*shift_to + uline(pat[:i+m-shift_to]) + pat[i+m-shift_to:])
+            print(' '*shift_to + uline(pat[:i+m-k-shift_to]) + pat[i+m-k-shift_to:])
             print()
-            print(txt[:shift_to] + uline(txt[shift_to:i+m]) + txt[i+m:])
+            print(' '*k + txt[:shift_to] + uline(txt[shift_to:i+m-k]) + txt[i+m-k:])
             sleep(1)
         i = shift_to
     return ''
